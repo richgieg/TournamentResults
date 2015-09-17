08567ae33d03207586f58aa70b19fcf4f3dbd8ef
Richard Gieg <richgieg@users.noreply.github.com>
2015-09-17 12:10:44 -0700

    Create matches table

--- tournament.sql
+++ tournament.sql
@@ -8,3 +8,10 @@ CREATE TABLE players (
     id serial primary key,
     name text
 );
+
+-- Create matches table.
+CREATE TABLE matches (
+    id serial primary key,
+    winner integer references players(id),
+    loser integer references players(id)
+);
