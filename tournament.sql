b2bc72188652efc87b9c9cf5148ae45cfe848d78
Richard Gieg <richgieg@users.noreply.github.com>
2015-09-17 15:48:10 -0700

    Add view for player matches

--- tournament.sql
+++ tournament.sql
@@ -22,3 +22,10 @@ SELECT players.id, COUNT(matches.winner) AS wins
 FROM players LEFT JOIN matches
 ON players.id = matches.winner
 GROUP BY players.id;
+
+-- Create view for finding number of matches per player.
+CREATE VIEW player_matches AS
+SELECT players.id, COUNT(matches.winner) AS matches
+FROM players LEFT JOIN matches
+ON players.id = matches.winner OR players.id = matches.loser
+GROUP BY players.id;
